507250e5934ba5cbaaeec7e13c9d355546201111
Duc Nguyen <ducngx95@gmail.com>
2019-08-30 04:48:10 +1000

    Modify module names in DSE model

--- model_dse.py
+++ model_dse.py
@@ -8,8 +8,8 @@ class Module_dSprites_VAE(nn.Module):
         self.filters_first_layer = filters_first_layer
         self.latent_dim = latent_dim
         super().__init__()
-        self.encoder = self.define_frames_encoder(latent_dim=self.latent_dim)
-        self.decoder = self.define_frames_decoder(latent_dim=self.latent_dim)
+        self.encode = self.define_frames_encoder(latent_dim=self.latent_dim)
+        self.decode = self.define_frames_decoder(latent_dim=self.latent_dim)
         self.init_weight()
 
     def init_weight(self):
@@ -19,7 +19,7 @@ class Module_dSprites_VAE(nn.Module):
                 nn.init.constant_(m.bias, 1)
             elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d) or isinstance(m, nn.Linear):
                 nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
-        nn.init.xavier_normal_(self.decoder[-2].weight, nn.init.calculate_gain('sigmoid'))
+        nn.init.xavier_normal_(self.decode[-2].weight, nn.init.calculate_gain('sigmoid'))
 
     def conv_unit(self, in_channels, out_channels, kernel_size, stride=1, padding=0, batch_norm=False,
                   activation_relu=True):
@@ -89,12 +89,12 @@ class Module_dSprites_VAE(nn.Module):
         return mu + eps * std
 
     def forward(self, x, no_dec=False):
-        x_encoded = self.encoder(x)
+        x_encoded = self.encode(x)
         z_mean = x_encoded[:, :self.latent_dim]
         z_logvar = x_encoded[:, self.latent_dim:]
         z = self.reparameterize(z_mean, z_logvar)
         if no_dec:
             return z.squeeze()
         else:
-            x_recon = self.decoder(z)
+            x_recon = self.decode(z)
             return x_recon, z_mean, z_logvar, z.squeeze()
